5a4919e452e51705c56827b45a61ff4bdd9533b1
xiawenke <xiawenke@C02FQ0WTMD6M.local>
2023-01-10 11:51:48 +0800

    feat: 初始化项目

---
+++ src/pages/User/Login/index.tsx
@@ -0,0 +1,159 @@
+import {login} from '@/services/ant-design-pro/api';
+import {AlipayCircleOutlined, TaobaoCircleOutlined, WeiboCircleOutlined,} from '@ant-design/icons';
+import {useEmotionCss} from '@ant-design/use-emotion-css';
+import {Helmet, history, useIntl, useModel} from '@umijs/max';
+import {Alert, message} from 'antd';
+import Settings from '../../../../config/defaultSettings';
+import React, {useState} from 'react';
+import {flushSync} from 'react-dom';
+import {SelectLang} from '@/components/Locale/SelectLang'
+import './login.scss'
+import whiteLogo from '@/assets/images/common/white-logo.png'
+const ActionIcons = () => {
+  const langClassName = useEmotionCss(({ token }) => {
+    return {
+      marginLeft: '8px',
+      color: 'rgba(0, 0, 0, 0.2)',
+      fontSize: '24px',
+      verticalAlign: 'middle',
+      cursor: 'pointer',
+      transition: 'color 0.3s',
+      '&:hover': {
+        color: token.colorPrimaryActive,
+      },
+    };
+  });
+
+  return (
+    <>
+      <AlipayCircleOutlined key="AlipayCircleOutlined" className={langClassName} />
+      <TaobaoCircleOutlined key="TaobaoCircleOutlined" className={langClassName} />
+      <WeiboCircleOutlined key="WeiboCircleOutlined" className={langClassName} />
+    </>
+  );
+};
+
+const Lang = () => {
+
+  const langClassName = useEmotionCss(({ token }) => {
+    console.log(token);
+    return {
+      height: 22,
+      lineHeight: '22px',
+      position: 'fixed',
+      top:60,
+      right: 88,
+    };
+  });
+
+  return (
+    <div className={langClassName} data-lang>
+      {SelectLang && <SelectLang />}
+    </div>
+  );
+};
+
+const LoginMessage: React.FC<{
+  content: string;
+}> = ({ content }) => {
+  return (
+    <Alert
+      style={{
+        marginBottom: 24,
+      }}
+      message={content}
+      type="error"
+      showIcon
+    />
+  );
+};
+
+const Login: React.FC = () => {
+  const [userLoginState, setUserLoginState] = useState<API.LoginResult>({});
+  const [type, setType] = useState<string>('account');
+  const { initialState, setInitialState } = useModel('@@initialState');
+
+  const containerClassName = useEmotionCss(() => {
+    return {
+      display: 'flex',
+      flexDirection: 'column',
+      height: '100vh',
+      overflow: 'auto',
+    };
+  });
+
+  const intl = useIntl();
+
+  const fetchUserInfo = async () => {
+    const userInfo = await initialState?.fetchUserInfo?.();
+    if (userInfo) {
+      flushSync(() => {
+        setInitialState((s) => ({
+          ...s,
+          currentUser: userInfo,
+        }));
+      });
+    }
+  };
+
+  const handleSubmit = async (values: API.LoginParams) => {
+    try {
+      // 登录
+      const msg = await login({ ...values, type });
+      if (msg.status === 'ok') {
+        const defaultLoginSuccessMessage = intl.formatMessage({
+          id: 'pages.login.success',
+          defaultMessage: '登录成功！',
+        });
+        message.success(defaultLoginSuccessMessage);
+        await fetchUserInfo();
+        const urlParams = new URL(window.location.href).searchParams;
+        history.push(urlParams.get('redirect') || '/');
+        return;
+      }
+      console.log(msg);
+      // 如果失败去设置用户错误信息
+      setUserLoginState(msg);
+    } catch (error) {
+      const defaultLoginFailureMessage = intl.formatMessage({
+        id: 'pages.login.failure',
+        defaultMessage: '登录失败，请重试！',
+      });
+      console.log(error);
+      message.error(defaultLoginFailureMessage);
+    }
+  };
+  const { status, type: loginType } = userLoginState;
+
+  return (
+    <div className={`${containerClassName} login-container`}>
+      <Helmet>
+        <title>
+          {intl.formatMessage({
+            id: 'menu.login',
+            defaultMessage: '登录页',
+          })}
+          - {Settings.title}
+        </title>
+      </Helmet>
+      <Lang />
+      <div className="login-container-wrapper">
+        <div className="left">
+          <div className="logo">
+            <img src={whiteLogo} alt=""/>
+          </div>
+          <div className="slogan">
+            All in One Platform. <br/>
+            All for Digital Assets.
+          </div>
+        </div>
+        <div className="right">
+
+        </div>
+      </div>
+      {/*<Footer />*/}
+    </div>
+  );
+};
+
+export default Login;
